e460f3ce4eb556ae1238d75a7b1d696c87af18b5
Greedy <greedy0110@gmail.com>
2017-01-14 14:23:15 +0900

    init commit

--- ChatWithUs/CWU.Designer.cs
+++ ChatWithUs/CWU.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChatWithUs
 {
-    partial class Form1
+    partial class CWU
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
--- ChatWithUs/CWU.cs
+++ ChatWithUs/CWU.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ChatWithUs
 {
-    public partial class Form1 : Form
+    public partial class CWU : Form
     {
-        public Form1()
+        public CWU()
         {
             InitializeComponent();
         }
